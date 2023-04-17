c9bb0c217a6a3b4cfe396e39bb6f166544010876
Anna <annajaninaschmidt@gmail.com>
2023-04-17 12:50:55 +1000

    file setup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 
+// import About from './components/About';
+// import Contact from './components/Contact';
+// import Footer from './components/Footer';
+// import Header from './components/Header';
+// import Navbar from './components/Navbar';
+// import Portfolio from './components/Portfolio';
+// import Resume from './components/Resume';
+
 function App() {
   return (
     <div className="App">
